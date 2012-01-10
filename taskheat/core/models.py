8bcae14037d57e3a251ac746e9ac98c6f4a461f8
Chris McDonald <XWraithanX@gmail.com>
2012-01-10 05:16:18 -0800

    add first pass at models

--- taskheat/core/models.py
+++ taskheat/core/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    name = models.CharField(max_length=255)
+    details = models.TextField()
+    max_weight = models.IntegerField()
+    min_weight = models.IntegerField()
+    decay_interval = models.IntegerField()
+
+    def temperature(self, when):
+        today = date.today()
+        start = today-timedelta(days=self.max_weight+-self.min_weight)
+        possible_ct = self.completedtask_set.filter(when__gte=start).order_by('-when')
+        if possible_ct.exists():
+            ct = possible_greens[0]
+            days_old = (today-ct.when).days
+            decayed_value = ct.weight + days_old*self.decay_interval
+            if self.decay_interval > 0:
+                if decayed_value < self.max_weight:
+                    return decayed_value
+                else:
+                    return self.max_weight
+            else:
+                if decayed_value > self.min_weight:
+                    return decayed_value
+                else:
+                    return self.min_weight
+        else:
+            if self.decay_interval > 0:
+                return self.max_weight
+            else:
+                return self.min_weight
+
+
+class CompletedTask(models.Model):
+    task = models.ForeignKey('Task')
+    weight = models.IntegerField()
+    when = models.DateField(auto_now_add=True)
--- taskheat/urls.py
+++ taskheat/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.views.generic.list import ListView
+from core.models import Task
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -6,7 +8,10 @@ from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'taskheat.views.home', name='home'),
+    url(r'^$',
+        ListView.as_view(allow_empty=True,
+                         model=Task),
+        name='core_tast_list'),
     # url(r'^taskheat/', include('taskheat.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
