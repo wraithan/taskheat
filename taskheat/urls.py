85a0ff59773fd7a4ff6d3524f14173863420229f
Chris McDonald <XWraithanX@gmail.com>
2012-01-29 14:15:20 -0800

    admin

--- taskheat/urls.py
+++ taskheat/urls.py
@@ -1,22 +1,18 @@
-from django.conf.urls.defaults import patterns, include, url
-from django.views.generic.list import ListView
 from core.models import Task
+from django.conf.urls.defaults import patterns, include, url
+from django.contrib import admin
+from django.views.generic.list import ListView
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
 
+admin.autodiscover()
 urlpatterns = patterns('',
-    # Examples:
     url(r'^$',
         ListView.as_view(allow_empty=True,
                          model=Task),
         name='core_tast_list'),
-    # url(r'^taskheat/', include('taskheat.foo.urls')),
+    url(r'^admin/',
+        include(admin.site.urls)),
+    url(r'^admin/doc/',
+        include('django.contrib.admindocs.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
 )
